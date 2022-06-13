e4b9d7e002e202182348cbdb31dbf8f2dfff7e48
erin <fix.society.github@gmail.com>
2022-06-13 22:43:06 +0200

    wire up lock buttons

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,43 +2,27 @@ import logo from './logo.svg';
 import './App.module.scss';
 import Button from '../Components/Button/Button';
 import InputBox from '../Components/InputBox/InputBox';
-import React from 'react';
-
-// let focusHandler = (e:React.FocusEvent) => {
-//   console.log(e);
-//   if(e.target.reado){
-//     var parent = e.target.parentElement.getElementsByTagName('label')[0].classList.add(`${classes.active}`);
-//   }
-// };
-// let blurHandler = (e:React.FocusEvent) => {
-//   console.log(e);
-//   if(e.target.readonly){
-//     var parent = e.target.parentElement.getElementsByTagName('label')[0].classList.remove(`${classes.active}`);
-//   }
-// };
+import React, { useState } from 'react';
 
 function App() {
- // let coinLocked: boolean = useSt8(false);
+  const [coinLocked, setCoinLocked] = useState(false);
+  const [pitchLocked, setpitchLocked] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
         <InputBox id='coin' readonly={false} title='coin name' onClick={(currentLocked) => {
-            console.log(currentLocked);
-          //  locked = currentLocked;
+            setCoinLocked(!coinLocked);
           }} 
-          locked={false} />
+          locked={coinLocked} />
         <InputBox id='pitch' readonly={true} title='sales pitch' onClick={(currentLocked) => {
-            console.log(currentLocked);
-        //    this.locked = !currentLocked;
+            setpitchLocked(!pitchLocked);
           }} 
-          locked={false}  />
+          locked={pitchLocked}  />
         <Button onClick={() => {console.log('clicked');}} text="randomize" />
-        <a
-          className="App-link"
+        <a className="App-link"
           href="https://github.com/the-erin-collective/lorem-shitcoin"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer" >
           get the code (AGPL3 licenced)
         </a>
       </header>
